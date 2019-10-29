38628afca849ec42c8ca2e89df43820c2c6cb7ba
JohnnJudge <56047717+JohnnJudge@users.noreply.github.com>
2019-10-29 16:03:24 -0700

    Update myProg01.cpp

--- myProg01.cpp
+++ myProg01.cpp
@@ -1,17 +1,17 @@
 // myProg.cpp
 // Author: John Judge
 #include <iostream>
+
 using namespace std;
- 
+
 int main() {
     // Simple for loop that counts down from n to 1
- 
-    int n=5;
- 
-    for (int i=n; i>0; i--) {
-       cout << i << " ";
+
+    int n = 5;
+
+    for (int i = n; i > 0; i--) {
+        cout << i << " ";
     }
- cout<< "\n";
+    cout << "\n";
     return 0;
 }
-
